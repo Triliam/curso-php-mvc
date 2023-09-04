e8ed699fcd8a0e7e119e3607251a14ce7951e274
Triliam <taisbuenovidotto@gmail.com>
2023-09-04 18:32:42 -0300

    php-mvc

---
+++ curso-php-mvc/app/views/confirmaDelete.php
@@ -0,0 +1,10 @@
+<div class="row container">
+    <div class="col s12">
+        <h3 class="light">Deseja deletar esse cadastro?</h3>
+
+        <p>
+            <a href="?router=Site/deletar/&id=<?php echo $id ?>" class="flow-text light red-text">SIM</a>
+            <a href="?router=Site/consulta" class="flow-text light red-text">NAO</a>
+        </p>
+    </div>
+</div>
---
+++ curso-php-mvc/app/views/galeria.php
@@ -0,0 +1,7 @@
+<div class="row container">
+    <div class="col s12">
+        <h1>galeria</h1>
+        <?php echo $photos ?>
+    </div>
+</div>
+   
